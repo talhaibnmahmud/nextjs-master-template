ac2833c1ced5265bd336ec4b97a2cc014dc12809
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-12-21 16:58:56 +0600

    🎉 Setup: Next.JS bundle analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
 import type { NextConfig } from "next";
 
 import "@/configs/env/client";
@@ -12,4 +13,9 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const isBundleAnalyzerEnabled = process.env.ANALYZE === "true";
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: isBundleAnalyzerEnabled,
+});
+
+export default isBundleAnalyzerEnabled ? withBundleAnalyzer(nextConfig) : nextConfig;
